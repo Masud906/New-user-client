4f1fc9289530e867ce7f705b931f63643f3362c1
masud rana <Masud Rana>
2024-02-15 23:19:44 +0600

    unnon error

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=> {
+    fetch('http://localhost:5000/users')
+ .then(res => res.json())
+ .then(data => useEffect();
+  },[])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user);
+  }
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h3>Number of users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="text" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.id} {user.name} {user.email}</p>)
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
